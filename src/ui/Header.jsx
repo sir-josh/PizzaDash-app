0bd795958614688821e9c6e0bd8b14d4edcb15d0
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-28 23:29:58 +0100

    feat: fetch order from API endpoint

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
 	return (
 		<header>
 			<Link to="/">PizzaDash Co.</Link>
-
+			<SearchOrder />
 			<p>Mirabel</p>
 		</header>
 	);
