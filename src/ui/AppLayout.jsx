650b7082a5d49e7fed77d202a76ef7e94fa6af59
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-28 22:54:15 +0100

    feat: display loader indicator on api request call

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import Header from "./Header";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Loader from "./Loader";
 
 const AppLayout = ({ children }) => {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+
 	return (
-		<div>
+		<div className="layout">
+            {isLoading && <Loader />}
+
 			<Header />
 
 			<main><Outlet /></main>
