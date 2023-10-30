d538f8381750db08a378a250a2e5ae298cb23c5c
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-30 03:02:13 +0100

    style: add style to loading indicator

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,12 @@
 function Loader() {
-	return <div className="loader"></div>;
+  return (
+    <div
+      className="absolute inset-0 flex items-center justify-center
+	 bg-slate-600/40 backdrop-blur-sm"
+    >
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
