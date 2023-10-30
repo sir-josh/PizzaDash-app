b55cfa81623c96b5406d41fbfa547624b9a7c78f
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-30 18:01:32 +0100

    feat: model the 'User' state with Redux Toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="text-sm font-semibold hidden sm:block">Mirabel</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return (
+    <div className="hidden text-sm font-semibold sm:block">{username}</div>
+  );
 }
 
 export default Username;
