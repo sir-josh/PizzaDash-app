2f3591014cb35b0818f4a67deba53b52e751ce14
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-30 15:11:50 +0100

    style: style the cart component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
-  const buttonBaseStyle = `focus: inline-block rounded-full bg-yellow-400 
-  font-bold uppercase tracking-wide text-stone-800 transition-colors 
-  hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring 
-  focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed 
+  const miniButtonStyle = `focus: inline-block rounded-full 
+    font-bold uppercase tracking-wide transition-colors 
+    focus:outline-none focus:ring focus:ring-offset-2 
+    disabled:cursor-not-allowed`;
+
+  const buttonBaseStyle = `${miniButtonStyle}  bg-yellow-400 
+    text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300  
+    focus:ring-yellow-300  
   `;
 
   const buttonStyles = {
     primary: buttonBaseStyle + " px-4 py-3 md:px-6 md:py-4",
-    small: buttonBaseStyle + " px-4 py-2 md:px-5 md:py-3 text-xs"
+    small: buttonBaseStyle + " px-4 py-2 md:px-5 md:py-3 text-xs",
+    secondary:
+      miniButtonStyle +
+      ` bg-transparent text-stone-800 focus:text-stone-800 
+      focus:ring-stone-200 focus:bg-stone-300 hover:bg-stone-400 
+      hover:text-stone-800 text-stone-400 border-2 border-stone-300 px-4 
+      py-2.5 md:px-6 md:py-3.5
+    `,
   };
 
   if (to)
