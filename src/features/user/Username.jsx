e892f5f626b0c7bdaf268e51618ea0c5ed9854d0
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-30 01:18:54 +0100

    style: add flexbox style to some components

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-    return <div className="text-sm font-semibold">Mirabel</div>;
+  return <div className="text-sm font-semibold hidden sm:block">Mirabel</div>;
 }
 
 export default Username;
