e811551a1e30a1ff346198684beb468cb3876016
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-30 12:53:50 +0100

    style: add tailwind css style to Menu Components

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className = `focus: inline-block rounded-full bg-yellow-400 px-4 py-3 
+function Button({ children, disabled, to, type }) {
+  const buttonBaseStyle = `focus: inline-block rounded-full bg-yellow-400 
   font-bold uppercase tracking-wide text-stone-800 transition-colors 
   hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring 
   focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed 
-  sm:px-6 sm:py-4`;
+  `;
+
+  const buttonStyles = {
+    primary: buttonBaseStyle + " px-4 py-3 md:px-6 md:py-4",
+    small: buttonBaseStyle + " px-4 py-2 md:px-5 md:py-3 text-xs"
+  };
 
   if (to)
     return (
-      <Link className={className} to={to}>
+      <Link className={buttonStyles[type]} to={to}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={buttonStyles[type]}>
       {children}
     </button>
   );
