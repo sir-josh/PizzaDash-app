9e598b311d73fd48a742d6222d3a60e6b040b389
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-31 00:53:37 +0100

    feat: build CartOverview component with Redux Selectors

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
+// {
+//   pizzaId: 12,
+//   name: "Mediterranean",
+//   quantity: 2,
+//   unitPrice: 16,
+//   totalPrice: 32,
+// },
 
 const cartSlice = createSlice({
   name: "cart",
@@ -41,6 +40,12 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 export const {
   addToCart,
