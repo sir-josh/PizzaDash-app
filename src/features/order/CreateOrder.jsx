776e19492d7ab33a8663d0f15642a3d5fe82bf56
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-30 02:12:59 +0100

    style: style order form elements

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -63,7 +63,14 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              className="w-full rounded-full border border-stone-200 px-4 py-2
+              text-sm transition-all duration-300 focus:outline-none 
+              focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
@@ -72,6 +79,8 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring 
+            focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -2,25 +2,29 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SearchOrder = () => {
-	const [query, setQuery] = useState("");
-	const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		if (!query) return;
-		navigate(`/order/${query}`);
-		setQuery("");
-	}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				value={query}
-				placeholder="Search your order(#) item here"
-				onChange={(e) => setQuery(e.target.value)}
-			/>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={query}
+        placeholder="Search your order(#) item here"
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 
+		text-sm transition-all duration-300 placeholder:text-stone-400 
+		sm:focus:w-72 sm:w-64 focus:outline-none focus:ring focus:ring-yellow-500
+		focus:ring-opacity-50"
+      />
+    </form>
+  );
 };
 
 export default SearchOrder;
