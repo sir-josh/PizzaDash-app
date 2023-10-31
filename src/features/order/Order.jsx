fd24e54f8bb8ec34a99fce6851814d2048b57c66
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-10-31 23:55:49 +0100

    feat: fetch Menu data on Order page component using useFetcher() hook

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -8,9 +8,22 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher(); //This is used to fetch data from another component file
+
+  useEffect(
+    function () {
+      //fetch data from menu page when fetcher state is idle and haven't fetch any data
+      if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+    },
+    [fetcher],
+  );
+
+  console.log(fetcher.data);
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -62,7 +75,14 @@ function Order() {
 
       <ul className=" divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher.data?.find((el) => el.id === item.pizzaId)?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
@@ -90,27 +110,3 @@ export async function loader({ params }) {
 }
 
 export default Order;
-
-// const fakeCart = [
-// 	{
-// 	  pizzaId: 12,
-// 	  name: "Mediterranean",
-// 	  quantity: 2,
-// 	  unitPrice: 16,
-// 	  totalPrice: 32,
-// 	},
-// 	{
-// 	  pizzaId: 6,
-// 	  name: "Vegetale",
-// 	  quantity: 1,
-// 	  unitPrice: 13,
-// 	  totalPrice: 13,
-// 	},
-// 	{
-// 	  pizzaId: 11,
-// 	  name: "Spinach and Mushroom",
-// 	  quantity: 1,
-// 	  unitPrice: 15,
-// 	  totalPrice: 15,
-// 	},
-//   ];
